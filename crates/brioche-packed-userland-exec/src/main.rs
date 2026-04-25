0139d710e359fc37c4875e2bb7bcca501d1a97af
Jérémy Audiger <31616285+jaudiger@users.noreply.github.com>
2026-04-25 06:45:28 +0200

    build: update Rust to 1.95 and nightly (#128)
    Signed-off-by: Jérémy Audiger <jeremy.audiger@icloud.com>

--- crates/brioche-packed-userland-exec/src/main.rs
+++ crates/brioche-packed-userland-exec/src/main.rs
@@ -3,8 +3,8 @@
 
 mod linux;
 
-cfg_if::cfg_if! {
-    if #[cfg(target_os = "linux")] {
+cfg_select! {
+    target_os = "linux" => {
         #[cfg_attr(not(test), unsafe(no_mangle))]
         #[allow(clippy::missing_safety_doc, clippy::similar_names)]
         pub unsafe extern "C" fn main(argc: libc::c_int, argv: *const *const libc::c_char) -> libc::c_int {
@@ -12,7 +12,8 @@ cfg_if::cfg_if! {
                 linux::entrypoint(argc, argv)
             }
         }
-    } else {
+    }
+    _ => {
         fn main() {
             eprintln!("brioche-packed-userland-exec is only supported on Linux");
             std::process::exit(1);
